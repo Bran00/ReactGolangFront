325f68f7702b1b33fae05f23e6fb85f9f232b235
bran00 <86134682+Bran00@users.noreply.github.com>
2023-09-10 17:12:23 -0400

    feat: improve the card component

---
+++ src/components/Card/cardJob.jsx
@@ -0,0 +1,22 @@
+
+import React from "react"
+import styles from "./cardJob.module.css"
+
+const VagaCard = ({ vaga }) => {
+  return (
+    <div className={styles.vagaCard}>
+      <h1 className={styles.titulo}>{vaga.jobname}</h1>
+      <p className={styles.descricao}>{vaga.description}</p>
+      <ul className={styles.tags}>
+        {vaga.tags.map((tag, index) => (
+          <li key={index} className={styles.tag}>
+            {tag}
+          </li>
+        ))}
+      </ul>
+      <a href={vaga.link} target="_blank">Ver Detalhes</a>
+    </div>
+  )
+}
+
+export default VagaCard
